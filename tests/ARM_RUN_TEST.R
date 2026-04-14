18f9a9e7d7804201a2110474591068f455353308
ericgiunta <egiunta@ksu.edu>
2026-04-14 14:55:37 -0500

    updated test to poisson failure

--- tests/ARM_RUN_TEST.R
+++ tests/ARM_RUN_TEST.R
@@ -1,24 +1,21 @@
 library(Colossus)
 library(data.table)
-df <- data.table::data.table(
-    'UserID' = c(112, 114, 213, 214, 115, 116, 117),
-    'Starting_Age' = c(18, 20, 18, 19, 21, 20, 18),
-    'Ending_Age' = c(30, 45, 57, 47, 36, 60, 55),
-    'Cancer_Status' = c(12, 10, 18, 6, 1, 11, 4),
-    'a' = c(0, 1, 1, 0, 1, 0, 1),
-    'b' = c(1, 1.1, 2.1, 2, 0.1, 1, 0.2),
-    'c' = c(10, 11, 10, 11, 12, 9, 11),
-    'd' = c(0, 0, 0, 1, 1, 1, 1)
-)
-
-df$pyr <- df$Ending_Age - df$Starting_Age
-a_n <- c(-0.75, 0.1, -0.05, -1.5)
-keep_constant <- c(0, 0, 0, 0)
-control <- list(
-"ncores" = 1, "lr" = 0.75, "maxiter" = 1, "halfmax" = 1, "epsilon" = 1e-3,
-"deriv_epsilon" = 1e-3, "step_max" = 0.2, "change_all" = TRUE,
-"thres_step_max" = 100.0, "verbose" = 4
-)
+library(survival)
+library(dplyr)
+data(cancer, package = "survival")
+veteran %>% setDT()
+df <- copy(veteran)
+# Make the same adjustments as Epicure example 6.5
+karno <- df$karno
+karno[93] <- 20
+df$karno <- karno / 100
+df$trt <- df$trt - 1
+df$trt <- as.integer(df$trt == 0)
+cell_lvl <- c("large", "squamous", "smallcell", "adeno")
+df$cell <- as.integer(factor(df$celltype, level = cell_lvl)) - 1
+df$karno50 <- df$karno - 50
+control <- list(ncores = 1, maxiter = 20, halfmax = 1, verbose = 4)
 #
-print("Poisson Run Start")
-poisres <- PoisRun(Pois(pyr, Cancer_Status) ~ loglinear(a, 0) + linear(b, c, 1) + plinear(d, 2), df, a_n = a_n, control = control, norm = "max")
+a_n <- c(-1, 1.17, -0.01)
+model <- Pois_Strata(time, status, cell) ~ loglinear(trt, 0) + loglin - dose(karno, 1) + PA()
+poisres <- PoisRun(model, df, a_n = a_n, control = control)
